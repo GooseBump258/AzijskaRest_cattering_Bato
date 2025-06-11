1f6405209722d2da2f2ffc416ac356fc6aae9631
Samuel Baťo <samo.bato6@gmail.com>
2025-06-11 12:12:42 +0200

    Pridanie triedy formular

---
+++ triedy/formular.php
@@ -0,0 +1,89 @@
+<?php
+// rezervacny_formular_original.php
+// Tento súbor obsahuje presne pôvodný HTML formulár pre rezerváciu stola, bez akýchkoľvek úprav.
+// Môžeš ho vkladať na akúkoľvek stránku pomocou include 'rezervacny_formular_original.php';
+?>
+
+<form id="form-submit" action="rezervuj.php" method="post">
+    <section id="book-table">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="heading">
+                        <h2>Booknite si stôl</h2>
+                    </div>
+                </div>
+                <div class="col-md-4 col-md-offset-2 col-sm-12">
+                    <div class="left-image">
+                        <img src="img/book_left_image.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="right-info">
+                        <h4>Rezervácia</h4>
+                        <form id="form-submit" action="" method="get">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <fieldset>
+                                        <select required name='day' onchange='this.form.()'>
+                                            <option value="">Vyberte si deň</option>
+                                            <option value="Pondelok">Pondelok</option>
+                                            <option value="Utorok">Utorok</option>
+                                            <option value="Streda">Streda</option>
+                                            <option value="Štvrtok">Štvrtok</option>
+                                            <option value="Piatok">Piatok</option>
+                                            <option value="Sobota">Sobota</option>
+                                            <option value="Nedeľa">Nedeľa</option>
+                                        </select>
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-6">
+                                    <fieldset>
+                                        <select required name='hour' onchange='this.form.()'>
+                                            <option value="">Vyberte si hodinu</option>
+                                            <option value="10-00">10:00</option>
+                                            <option value="12-00">12:00</option>
+                                            <option value="14-00">14:00</option>
+                                            <option value="16-00">16:00</option>
+                                            <option value="18-00">18:00</option>
+                                            <option value="20-00">20:00</option>
+                                            <option value="22-00">22:00</option>
+                                        </select>
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-6">
+                                    <fieldset>
+                                        <input name="name" type="name" class="form-control" id="name" placeholder="Full name" required="">
+                                    </fieldset> 
+                                </div>
+                                <div class="col-md-6">
+                                    <fieldset>
+                                        <input name="phone" type="phone" class="form-control" id="phone" placeholder="Phone number" required="">
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-6">
+                                    <fieldset>
+                                        <select required class="person" name='persons' onchange='this.form.()'>
+                                            <option value="">Koľko ľudí?</option>
+                                            <option value="1-Person">1 </option>
+                                            <option value="2-Persons">2 </option>
+                                            <option value="3-Persons">3 </option>
+                                            <option value="4-Persons">4 </option>
+                                            <option value="5-Persons">5 </option>
+                                            <option value="6-Persons">6 </option>
+                                        </select>
+                                    </fieldset>
+                                </div>
+                                <div class="col-md-6">
+                                    <fieldset>
+                                        <button type="submit" id="form-submit" class="btn">Booknite si stôl</button>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</form>
