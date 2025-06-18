a559d9040fdc95f69216f11cf5dbca355284440a
Samuel Baťo <samo.bato6@gmail.com>
2025-06-18 20:21:41 +0200

    a

--- rezervacie/rezervuj.php
+++ rezervacie/rezervuj.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'db_config.php';
+require_once 'triedy/db_config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $den = isset($_POST['day']) ? $_POST['day'] : '';
