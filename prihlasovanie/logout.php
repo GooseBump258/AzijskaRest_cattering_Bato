b1dbeaa22109980cb97678758be37d16e8001d46
Samuel Baťo <samo.bato6@gmail.com>
2025-06-19 13:33:37 +0200

    posledny push.

--- prihlasovanie/logout.php
+++ prihlasovanie/logout.php
@@ -4,13 +4,10 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// Uvoľní všetky premenné relácie
 session_unset();
 
-// Zničí reláciu
 session_destroy();
 
-// Presmeruje používateľa na domovskú stránku
 header('Location: ../index.php');
 exit();
 ?>
