d0db1d5f858153a58f11ac02052463ec012c55a3
Samuel Baťo <samo.bato6@gmail.com>
2025-06-19 12:41:48 +0200

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -1,14 +1,26 @@
 <?php
-require_once(__DIR__ . '/triedy/db_config.php'); 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require_once '/triedy/db_config.php';
 
-$meno = $_POST['name'];
-$email = $_POST['email'];
-$telefon = $_POST['phone'];
-$sprava = $_POST['message'];
+    $meno = $_POST['name'];
+    $email = $_POST['email'];
+    $telefon = $_POST['phone'];
+    $sprava = $_POST['message'];
+    $datum = date("Y-m-d H:i:s");
 
-$sql = "INSERT INTO kontakty (meno, email, telefon, sprava)
-        VALUES ('$meno', '$email', '$telefon', '$sprava')";
+    $sql = "INSERT INTO kontakty (meno, email, telefon, sprava, datum) 
+            VALUES ('$meno', '$email', '$telefon', '$sprava', '$datum')";
 
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('Správa bola úspešne odoslaná.'); window.location.href='contact.php';</script>";
+    } else {
+        echo "Chyba: " . $sql . "<br>" . $conn->error;
+    }
 
-
+    $conn->close();
+} else {
+    header("Location: kontakt.php");
+    exit();
+}
 ?>
+
